b1d65fb530257e26e29130b7e42d8e708ad344db
Dm3583 <pankovdm@gmail.com>
2021-03-27 05:05:05 -0700

    edit input fields

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -58,6 +58,9 @@ class ContactForm extends Component {
             value={name}
             onChange={handleInput}
             name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
           />
         </label>{' '}
         <label>
@@ -68,6 +71,9 @@ class ContactForm extends Component {
             value={number}
             onChange={handleInput}
             name="number"
+            pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+            title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+            required
           />
         </label>
         <div className="ContactForm__btnWrapper">
